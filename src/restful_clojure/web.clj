13348e73544f1620da40c262095f7cbc1615a6a6
tigerhy1 <tigerhy1@gmail.com>
2016-07-20 18:19:54 +0800

    add get-add-user functionality

--- src/restful_clojure/web.clj
+++ src/restful_clojure/web.clj
@@ -22,15 +22,38 @@
 (def test-cb 
     (c/get-doc bucket "hello"))
     ;(prn "test-cb"))
-             
+
+(defn get-uid
+    [unionId]
+    (let [k (str "unionid2uid" "_" unionId)
+          content (c/get-doc bucket k)]
+      (prn content)
+      (if (nil? content) 0 (:uid content))
+    ))
+
+(defn add-user
+    []
+    2)
+
+(defn get-add-user-db 
+    [openId]
+    (let [uid (get-uid openId)]
+    ;(prn "in get-add-user-db")
+    ;add-user add () around, then mean apply the function, 
+    ;otherwise just function object.
+    (cond (= 0 uid) (add-user)
+          :else uid)))
+
+
 (defn get-add-user [req]
     (let [body (:body req)
           openId (:openId body)
           unionId (:unionId body)
-          weixinName (:weixinName body)]
-    (prn test-cb)
+          weixinName (:weixinName body)
+          uid (get-add-user-db openId)]
+    ;(prn (get-add-user-db openId unionId weixinName))
     (prn openId)
-    (response {:openId openId}))
+    (response {:openId uid}))
     )
 
 (defn composer
